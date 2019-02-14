5f6fc24f8b897ca3e15050f72a741b9e66a2831b
Kamil Szafranek <szafranek.kamil@gmail.com>
2019-02-14 14:47:46 +0100

    Updates

---
+++ precache-manifest.a145294ed844e8e0ac30e5d111df3ded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4313e61c4ac004d57dd0",
+    "url": "/game-of-life/build/static/js/runtime~main.4313e61c.js"
+  },
+  {
+    "revision": "6d72f463123fd7f1ee2e",
+    "url": "/game-of-life/build/static/js/main.6d72f463.chunk.js"
+  },
+  {
+    "revision": "a94a29bc37d2b2b68949",
+    "url": "/game-of-life/build/static/js/1.a94a29bc.chunk.js"
+  },
+  {
+    "revision": "6d72f463123fd7f1ee2e",
+    "url": "/game-of-life/build/static/css/main.a9718e26.chunk.css"
+  },
+  {
+    "revision": "83d321b6ed479f4daeb9d4d7f2664bc0",
+    "url": "/game-of-life/build/index.html"
+  }
+];
