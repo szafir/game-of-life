f3ed2b6a565999f21b15a1487fdf5638af7f3761
Kamil Szafranek <szafranek.kamil@gmail.com>
2019-02-14 16:01:44 +0100

    Updates

--- precache-manifest.03cd38bb3f2cb4b472bbbfb71166e1e4.js
+++ precache-manifest.03cd38bb3f2cb4b472bbbfb71166e1e4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
   },
   {
-    "revision": "d6a80b4d642e8a82f654",
-    "url": "/game-of-life/static/js/main.d6a80b4d.chunk.js"
+    "revision": "431736f869c49d1aa3bb",
+    "url": "/game-of-life/static/js/main.431736f8.chunk.js"
   },
   {
     "revision": "a94a29bc37d2b2b68949",
     "url": "/game-of-life/static/js/1.a94a29bc.chunk.js"
   },
   {
-    "revision": "d6a80b4d642e8a82f654",
+    "revision": "431736f869c49d1aa3bb",
     "url": "/game-of-life/static/css/main.a9718e26.chunk.css"
   },
   {
-    "revision": "01d5684a2c30432e679642e7b17f3279",
+    "revision": "b3994eaa364856b1cd4f369128bc1329",
     "url": "/game-of-life/index.html"
   }
 ];
